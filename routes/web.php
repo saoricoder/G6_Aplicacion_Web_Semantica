83ddcfe0a2bbad469dc22a2359b69bff3fcfa44f
VictorHug56 <135287598+VictorHug56@users.noreply.github.com>
2025-12-14 22:04:34 -0500

    Rutas

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\MedicoController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route:: resource('medicos', MedicoController::class);
